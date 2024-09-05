50ccbdefaa25f7c030035f810b48ed4a4a9bb80e
Robin Read <robin.g.read@gmail.com>
2024-09-05 20:15:27 +0100

    Added src/libboids with initial CMakeLists.txt file, boids.h and boids.cpp files.

---
+++ src/libboids/boids.cpp
@@ -0,0 +1,77 @@
+#include "boids.h"
+#include "utils.h"
+#include <math.h>
+#include <QtMath>
+
+
+namespace boids {
+
+Boid::Boid(const uint16_t &id) : m_id(id)
+{
+    m_position.setX(0.0f);
+    m_position.setY(0.0f);
+    m_velocity.setX(0.0f);
+    m_velocity.setY(0.0f);
+}
+
+
+Boid::Boid(const uint16_t &id, const float x, const float y)  : m_id(id)
+{
+    m_position.setX(x);
+    m_position.setY(y);
+
+    const float maxVelocity = 5.0;
+    m_velocity = utils::generateRandomVelocityVector(maxVelocity);
+
+    const int r = utils::generateRandomValue<int>(50, 200);
+    const int g = utils::generateRandomValue<int>(100, 255);
+    const int b = utils::generateRandomValue<int>(100, 255);
+    m_color = QColor(r, g, b, 255);
+}
+
+
+Boid::Boid(const uint16_t &id, const float x, const float y, const float dx, const float dy)  : m_id(id)
+{
+    m_position.setX(x);
+    m_position.setY(y);
+    m_velocity.setX(dx);
+    m_velocity.setY(dy);
+}
+
+
+float Boid::getAngle() const
+{
+    return std::atan2(m_velocity.y(), m_velocity.x());
+}
+
+
+QColor Boid::getColor() const
+{
+    return m_color;
+}
+
+
+uint16_t Boid::getId() const
+{
+    return m_id;
+}
+
+
+QPointF Boid::getPosition() const
+{
+    return m_position;
+}
+
+
+QVector2D Boid::getVelocity() const
+{
+    return m_velocity;
+}
+
+
+void Boid::setPosition(const QPointF &pos)
+{
+    m_position = pos;
+}
+
+};
---
+++ src/libboids/boids.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <QColor>
+#include <QVector2D>
+#include <QPoint>
+
+namespace boids {
+
+/**
+ * @brief The Boid class contains the data pertaining to a single Boid.
+ * That is, the ID, the colour, the current position and heading and the current velocity.
+ */
+class Boid
+{
+public:
+    /**
+     * @brief Construct a new Boid object at location 0.0, 0.0, with not velocity.
+     * @param id ID to assign to the Boid.
+     */
+    Boid(const uint16_t &id);
+
+    /**
+     * @brief Construct a new Boid object at a given location with a given ID.
+     * @param id ID to assign to the Boid.
+     * @param x X screen coordinate.
+     * @param y Y screen coordinate.
+     */
+    Boid(const uint16_t &id, const float x, const float y);
+
+    /**
+     * @brief Construct a new Boid object at a given location, with a given velocity and ID.
+     * 
+     * @param id ID to assign to the Boid.
+     * @param x X screen coordinate.
+     * @param y Y screen coordinate.
+     * @param dx X velocity.
+     * @param dy Y velocity.
+     */
+    Boid(const uint16_t &id, const float x, const float y, const float dx, const float dy);
+
+    /**
+     * @brief Get the heading angle of the boid. This will be in the range (-PI, PI) and 
+     * is calculated from the Boid's velocity vector..
+     * @return Heading angle in radians.
+     */
+    float getAngle() const;
+
+    /**
+     * @brief Get the RGB colour of the Boid
+     * @return Colour
+     */
+    QColor getColor() const;
+
+    /**
+     * @brief Get the Boid ID.
+     * @return Boid ID.
+     */
+    uint16_t getId() const;
+
+    /**
+     * @brief Get the current position of the Boid.
+     * @return Position in the scene.
+     */
+    QPointF getPosition() const;
+
+    /**
+     * @brief Get the current velocity of the Boid.
+     * @return Velocity in the scene.
+     */
+    QVector2D getVelocity() const;
+
+    /**
+     * @brief Set the Position of the Boid.
+     * @param pos Position in the format (x, y).
+     */
+    void setPosition(const QPointF &pos);
+
+private:
+    uint16_t m_id;
+    QColor m_color;
+    QPointF m_position;
+    QVector2D m_velocity;
+};
+
+};
