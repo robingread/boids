43675e86b65e0db776da52d88667c529550ede20
Robin Read <robin.g.read@gmail.com>
2024-09-05 20:47:54 +0100

    Added initial code for the main app, which uses the libboids and libboids_gui libraries. Updated the top-level CMakeLists.txt file to include the src/app subdirectory.

---
+++ src/app/main.cpp
@@ -0,0 +1,12 @@
+#include "../gui/dialog.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Dialog w;
+    w.show();
+    w.run();
+    return a.exec();
+}
