53626405a214c4d9cac1229b4db0544ec2a85b5c
Robin Read <robin.g.read@gmail.com>
2024-09-06 22:46:57 +0100

    Added stub Config class to the libboids library in the config.h file.

---
+++ src/libboids/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace boids {
+
+struct Config {
+    float maxVelocity         = 2.0f;
+    float neighbourhoodRadius = 100.0f;
+
+    float alignmentScale = 0.01;
+    float coheasionScale = 0.05;
+    float repelScale     = 0.1;
+};
+} // namespace boids
