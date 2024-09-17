808e569d4eb89e82c22f613e0f455a66fbee084b
Robin Read <robin.g.read@gmail.com>
2024-09-17 19:03:29 +0100

    Adding docstrings for the Boid class in the gui/ folder.

--- src/gui/boid.h
+++ src/gui/boid.h
@@ -5,6 +5,11 @@
 #include <QPainter>
 #include <QPointF>
 
+/**
+ * @brief The Boid class is used to render Boid in the GUI. It inherits from the
+ * QGraphicsItem class and thus can be added to a QGraphicsScene but provides a custom
+ * implementation for the boundingRect() and paint() methods.
+ */
 class Boid : public QGraphicsItem {
   public:
     Boid(const QPointF& pos, const float rot, const QColor& colour);
