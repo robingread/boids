5aa12421100660598c9d33ac1867a20c940a68e8
Robin Read <robin.g.read@gmail.com>
2024-09-17 09:26:24 +0100

    Updated the libboids/flock.cpp files to include Obstacle avoidance in the Flock::update() method. This now calculates the neighbourhood of the obstacles around a Boid and calculated the separation vector the leads to obstacle avoidance.

--- src/libboids/flock.cpp
+++ src/libboids/flock.cpp
@@ -32,10 +32,14 @@ QRectF Flock::getSceneBounds() const { return m_sceneBounds; }
 void Flock::setSceneBounds(const QRectF& bounds) { m_sceneBounds = bounds; }
 
 void Flock::update() {
-    std::vector<Boid>& boids = m_boidMap[BoidType::BOID];
+    std::vector<Boid>&       boids     = m_boidMap[BoidType::BOID];
+    const std::vector<Boid>& obstacles = m_boidMap[BoidType::OBSTACLE];
     for (Boid& b : boids) {
         const std::vector<boids::Boid> neighbours = boids::utils::getBoidNeighbourhood(
             b, boids, m_config.neighbourhoodRadius, m_sceneBounds);
+
+        const std::vector<boids::Boid> obstacleNeighbours = boids::utils::getBoidNeighbourhood(
+            b, obstacles, m_config.neighbourhoodRadius, m_sceneBounds);
 
         const QVector2D alignVector =
             boids::utils::calculateAlignmentVector(b, neighbours).normalized() *
@@ -49,11 +53,16 @@ void Flock::update() {
             boids::utils::calculateSeparationVector(b, neighbours, 75.0f).normalized() *
             m_config.repelScale;
 
+        const QVector2D obstacleVec =
+            boids::utils::calculateSeparationVector(b, obstacleNeighbours, 150.0f).normalized() *
+            m_config.repelScale * 2.0f;
+
         const QVector2D noiseVec = boids::utils::generateRandomVelocityVector(0.0001f);
 
         const QPointF& p = b.getPosition();
 
-        QVector2D v = b.getVelocity() + alignVector + cohesionVector + repelVec + noiseVec;
+        QVector2D v =
+            b.getVelocity() + alignVector + cohesionVector + repelVec + obstacleVec + noiseVec;
 
         boids::utils::clipVectorMangitude(v, m_config.maxVelocity);
 
