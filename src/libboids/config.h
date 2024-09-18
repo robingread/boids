c190bad5352ff177e9e90d0c5b3a8e7c2c5c2e6a
Robin Read <robin.g.read@gmail.com>
2024-09-18 21:32:41 +0100

    Added the obstacleRepelScale and predatorRepelScale values to the Boids::Config struct in the libboids/config.h file.

--- src/libboids/config.h
+++ src/libboids/config.h
@@ -6,8 +6,10 @@ struct Config {
     float maxVelocity         = 2.0f;
     float neighbourhoodRadius = 100.0f;
 
-    float alignmentScale = 0.1;
-    float coheasionScale = 0.05;
-    float repelScale     = 0.1;
+    float alignmentScale     = 0.1f;
+    float coheasionScale     = 0.05f;
+    float repelScale         = 0.1f;
+    float obstacleRepelScale = 2.0f;
+    float predatorRepelScale = 5.0f;
 };
 } // namespace boids
