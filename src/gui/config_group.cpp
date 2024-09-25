0fc514352872204bf98ce116d9825e06c83671a5
Robin Read <robin.g.read@gmail.com>
2024-09-25 13:33:06 +0100

    Added the ui::ConfigGroup class in the src/gui/config_group.h and .cpp files.

---
+++ src/gui/config_group.cpp
@@ -0,0 +1,50 @@
+#include "config_group.h"
+#include <iostream>
+
+namespace ui {
+
+ConfigGroup::ConfigGroup(const QString& name, QWidget* parent) : QWidget(parent) {
+    m_layout = std::make_unique<QVBoxLayout>(this);
+
+    m_nRadSlider      = createSlider("N Radius", 0.0f, 200.0f);
+    m_maxVelSlider    = createSlider("Max Vel", 0.0f, 3.0f);
+    m_alignSlider     = createSlider("Align", 0.0f, 1.0f);
+    m_cohesionSlider  = createSlider("Cohesion", 0.0f, 1.0f);
+    m_repelSlider     = createSlider("Repel", 0.0f, 1.0f);
+    m_obsRepelSlider  = createSlider("Obs Repel", 0.0f, 5.0f);
+    m_predRepelSlider = createSlider("Pred Repel", 0.0f, 10.0f);
+    m_repelMinDist    = createSlider("Repel Min Dist", 0.0f, 200.0f);
+
+    m_groupBox = std::make_unique<QGroupBox>(this);
+    m_groupBox->setTitle(name);
+    m_groupBox->setLayout(m_layout.get());
+
+    QVBoxLayout* l = new QVBoxLayout(this);
+    l->addWidget(m_groupBox.get());
+    l->geometry();
+}
+
+boids::Config ConfigGroup::getConfig() const {
+    boids::Config cfg;
+    cfg.neighbourhoodRadius = m_nRadSlider->getValue();
+    cfg.maxVelocity         = m_maxVelSlider->getValue();
+    cfg.alignmentScale      = m_alignSlider->getValue();
+    cfg.coheasionScale      = m_cohesionSlider->getValue();
+    cfg.repelScale          = m_repelSlider->getValue();
+    cfg.obstacleRepelScale  = m_obsRepelSlider->getValue();
+    cfg.predatorRepelScale  = m_predRepelSlider->getValue();
+    cfg.repelMinDist        = m_repelMinDist->getValue();
+    return cfg;
+}
+
+std::unique_ptr<Slider> ConfigGroup::createSlider(const QString& name, const float& minValue,
+                                                  const float& maxValue) {
+    std::unique_ptr<Slider> ptr = std::make_unique<Slider>(name, minValue, maxValue, this);
+    QObject::connect(ptr.get(), &Slider::valueChanged, this, &ConfigGroup::onSliderValueChanged);
+    m_layout->addWidget(ptr.get());
+    return ptr;
+}
+
+void ConfigGroup::onSliderValueChanged() { this->configChanged(); }
+
+} // namespace ui
---
+++ src/gui/config_group.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "config.h"
+#include "slider.h"
+#include <QGroupBox>
+#include <QString>
+#include <QVBoxLayout>
+#include <QWidget>
+
+namespace ui {
+
+/**
+ * @brief The ConfigGroup class is a GUI related clas created for conveniance. It creates a
+ * QGroupBox with a vertical layout containing Sliders that manage individual parts of a
+ * boids::Config object.
+ *
+ * All the sliders are stored internally, and the current configuration can be retreived by calling
+ * the `getConfig()` method.
+ */
+class ConfigGroup : public QWidget {
+
+    Q_OBJECT
+
+  public:
+    ConfigGroup(const QString& name, QWidget* parent = nullptr);
+
+    /**
+     * @brief Get the overall configuration as a boids::Config object.
+     * @return boids::Config instance.
+     */
+    boids::Config getConfig() const;
+
+  private:
+    std::unique_ptr<QGroupBox>   m_groupBox;
+    std::unique_ptr<QVBoxLayout> m_layout;
+
+    std::unique_ptr<Slider> m_nRadSlider;
+    std::unique_ptr<Slider> m_maxVelSlider;
+    std::unique_ptr<Slider> m_alignSlider;
+    std::unique_ptr<Slider> m_cohesionSlider;
+    std::unique_ptr<Slider> m_repelSlider;
+    std::unique_ptr<Slider> m_obsRepelSlider;
+    std::unique_ptr<Slider> m_predRepelSlider;
+    std::unique_ptr<Slider> m_repelMinDist;
+
+    /**
+     * @brief Create a Slider object, connect it to the onSliderValueChanged() callback, and add it
+     * to the Vertical Layout.
+     * @param name Name/title to display for the Slider label.
+     * @return Unique pointer to the Slider.
+     */
+    std::unique_ptr<Slider> createSlider(const QString& name, const float& minValue,
+                                         const float& maxValue);
+
+  signals:
+    /**
+     * @brief Signal emitted when there has been a change to any of the sliders in this object.
+     */
+    void configChanged();
+
+  private slots:
+    /**
+     * @brief Callback method/slot that emits a configChanged() signal.
+     */
+    void onSliderValueChanged();
+};
+} // namespace ui
