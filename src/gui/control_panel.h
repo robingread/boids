fec560d536a7e76de7565fcb679654e8e0971151
Robin Read <robin.g.read@gmail.com>
2024-09-26 11:46:04 +0100

    Updated the ui::ControlPanel class to inclde and instance of the ui::ButtonGroup as the m_buttonGroup member.

--- src/gui/control_panel.h
+++ src/gui/control_panel.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "button_group.h"
 #include "config_group.h"
 #include <QWidget>
 
@@ -10,6 +11,7 @@ class ControlPanelWidget : public QWidget {
     Q_OBJECT
 
   public:
+    std::unique_ptr<ButtonGroup> m_buttonGroup;
     std::unique_ptr<ConfigGroup> m_boidCfgGroup;
     std::unique_ptr<ConfigGroup> m_predatorCfgGroup;
     ControlPanelWidget(QWidget* parent = nullptr);
