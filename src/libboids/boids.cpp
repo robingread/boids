5f670c03b1da5c436323e9b12d165ad1945d9e3e
Robin Read <robin.g.read@gmail.com>
2025-01-17 20:06:36 +0000

    Updating the naming of the private member variables in the boids::Boid class.

--- src/libboids/boids.cpp
+++ src/libboids/boids.cpp
@@ -5,52 +5,52 @@
 
 namespace boids {
 
-Boid::Boid(const uint16_t& id, const BoidType type) : m_id(id), m_type(type) {
-    m_position.setX(0.0f);
-    m_position.setY(0.0f);
-    m_velocity.setX(0.0f);
-    m_velocity.setY(0.0f);
+Boid::Boid(const uint16_t& id, const BoidType type) : id_(id), type_(type) {
+    position_.setX(0.0f);
+    position_.setY(0.0f);
+    velocity_.setX(0.0f);
+    velocity_.setY(0.0f);
 }
 
 Boid::Boid(const uint16_t& id, const float x, const float y, const BoidType type)
-    : m_id(id), m_type(type) {
-    m_position.setX(x);
-    m_position.setY(y);
+    : id_(id), type_(type) {
+    position_.setX(x);
+    position_.setY(y);
 
     const float maxVelocity = 5.0;
-    m_velocity              = utils::generateRandomVelocityVector(maxVelocity);
+    velocity_               = utils::generateRandomVelocityVector(maxVelocity);
 
     const int r = utils::generateRandomValue<int>(50, 200);
     const int g = utils::generateRandomValue<int>(100, 255);
     const int b = utils::generateRandomValue<int>(100, 255);
-    m_color     = QColor(r, g, b, 255);
+    color_      = QColor(r, g, b, 255);
 }
 
 Boid::Boid(const uint16_t& id, const float x, const float y, const float dx, const float dy,
            const BoidType type)
-    : m_id(id), m_type(type) {
-    m_position.setX(x);
-    m_position.setY(y);
-    m_velocity.setX(dx);
-    m_velocity.setY(dy);
+    : id_(id), type_(type) {
+    position_.setX(x);
+    position_.setY(y);
+    velocity_.setX(dx);
+    velocity_.setY(dy);
 }
 
-float Boid::getAngle() const { return std::atan2(m_velocity.y(), m_velocity.x()); }
+float Boid::getAngle() const { return std::atan2(velocity_.y(), velocity_.x()); }
 
-QColor Boid::getColor() const { return m_color; }
+QColor Boid::getColor() const { return color_; }
 
-uint16_t Boid::getId() const { return m_id; }
+uint16_t Boid::getId() const { return id_; }
 
-QPointF Boid::getPosition() const { return m_position; }
+QPointF Boid::getPosition() const { return position_; }
 
-BoidType Boid::getType() const { return m_type; }
+BoidType Boid::getType() const { return type_; }
 
-QVector2D Boid::getVelocity() const { return m_velocity; }
+QVector2D Boid::getVelocity() const { return velocity_; }
 
-void Boid::setColor(const QColor& colour) { m_color = colour; }
+void Boid::setColor(const QColor& colour) { color_ = colour; }
 
-void Boid::setPosition(const QPointF& pos) { m_position = pos; }
+void Boid::setPosition(const QPointF& pos) { position_ = pos; }
 
-void Boid::setVelocity(const QVector2D& vel) { m_velocity = vel; }
+void Boid::setVelocity(const QVector2D& vel) { velocity_ = vel; }
 
 }; // namespace boids
