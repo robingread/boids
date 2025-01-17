873becc3143ebd6e67bc3fe5c8a47f198f8701b2
Robin Read <robin.g.read@gmail.com>
2025-01-17 19:52:41 +0000

    Updating the test/main.cpp file to run both GTest tests and Catch tests.

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,15 @@
-#include "gtest/gtest.h"
+#define CATCH_CONFIG_RUNNER // Prevent Catch2 from defining main()
+#include <catch2/catch.hpp>
+#include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
+    // Initialize Google Test
     ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    int gtest_result = RUN_ALL_TESTS();
+
+    // Run Catch2 tests
+    int catch_result = Catch::Session().run(argc, argv);
+
+    // Return 0 only if both test frameworks pass
+    return (gtest_result == 0 && catch_result == 0) ? 0 : 1;
 }
